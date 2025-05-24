2d1010e5225a9ae713f3e060e6d94193b142d394
Saibha69 <sadiasultanashefu@gmail.com>
2025-05-24 15:51:55 +0600

    Update A. Winter Sale.cpp

--- Cpp.Contest1/A. Winter Sale.cpp
+++ Cpp.Contest1/A. Winter Sale.cpp
@@ -2,7 +2,7 @@
 2.// Course title: Competitive Programming Sessional
 3.// Course code: CCE-2310
 4.// Course teacher name: Mirza Raquib
-5.// Date: 5/5/25
+5.// Date: 24/5/2025
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
