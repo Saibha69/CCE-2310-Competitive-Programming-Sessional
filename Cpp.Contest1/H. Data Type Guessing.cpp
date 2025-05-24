2b756ac0cd26f9acb830eea143ede67db900225f
Saibha69 <sadiasultanashefu@gmail.com>
2025-05-24 15:53:34 +0600

    Update H. Data Type Guessing.cpp

--- Cpp.Contest1/H. Data Type Guessing.cpp
+++ Cpp.Contest1/H. Data Type Guessing.cpp
@@ -1,3 +1,8 @@
+1.// Problem name: (I) Palindrome.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 24/5/2025
 #include<bits/stdc++.h>
 using namespace std;
 
