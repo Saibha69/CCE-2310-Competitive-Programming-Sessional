17678320f02a3e9c31f9a2352d6287ae2e9ffaf2
Saibha69 <sadiasultanashefu@gmail.com>
2025-05-26 01:20:43 +0600

    Create 2N.cpp

---
+++ Cpp.sheet 2/2N.cpp
@@ -0,0 +1,25 @@
+1.// Problem name: (N)
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 5/5/25
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s;
+    cin>>s;
+    long long n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        long long j;
+        cin>>j;
+        for(int k=1; k<=j; k++)
+        {
+            cout<<s;
+        }
+        cout<<endl;
+    }
+}
