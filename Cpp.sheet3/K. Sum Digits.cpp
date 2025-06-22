9358cc160ea35cedc96bf5dd40ed546a6dd2c1db
Saibha69 <sadiasultanashefu@gmail.com>
2025-06-22 23:03:21 +0600

    Create K. Sum Digits.cpp

---
+++ Cpp.sheet3/K. Sum Digits.cpp
@@ -0,0 +1,19 @@
+1.// Problem name:  k.Sum Digits
+2.//Course title: Competitive Programming Sessional 
+3.//Course code: 2310
+4.// Course teacher name: Mirza Raquib
+5.//Date: 022/6/25
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long n;
+     cin>>n;
+     string s;
+     cin>>s;                                                                                                                                                                                                                             long long sum=0;
+    for(int i=0;i<n;i++)
+        sum= sum +(int)(s[i]-'0');
+        cout<<sum<<endl;
+
+}
